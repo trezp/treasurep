f320381d7e626005881736ae1e57c4de886d2b52
Treasure Porth <treasure.rose@gmail.com>
2015-02-05 13:51:12 -0800

    Added jQuery, fixed fonts

---
+++ portfolio.js
@@ -0,0 +1,21 @@
+
+
+
+
+// $('document').ready(function(){
+// 	var animatedArray = ['<span class= "switch1">Web Developer</span>', '<span class= "switch1">JavaScript Admirer</span>',
+// 	'<span class= "switch1">Meteor.js Enthusiast</span>'];
+
+// 	function insertSpan(arrayItem){
+// 	 $('#aboutMe').html(arrayItem).fadeOut(1000)
+// }
+
+// 	for(i = 0; i < animatedArray.length; i++){
+// 		(function(index){
+// 			insertSpan(animatedArray + index)
+// 		})(i);
+		
+// 	}
+
+// });
+	

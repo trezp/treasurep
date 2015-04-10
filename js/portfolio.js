db1c7c36518c99957854693c108a45fee9f839e2
Treasure Porth <treasure.rose@gmail.com>
2015-04-10 12:54:12 -0700

    Added new proj descriptions.

--- js/portfolio.js
+++ js/portfolio.js
@@ -22,18 +22,18 @@ $('document').ready(function(){
 */
 
 // init controller
-var controller = new ScrollMagic();
+// var controller = new ScrollMagic();
 
-// assign handler "scene" and add it to controller
-var scene = new ScrollScene({duration: 100})
-                .setPin("#my-sticky-element") // pins the element for a scroll distance of 100px
-                .addTo(controller); // add scene to controller
+// // assign handler "scene" and add it to controller
+// var scene = new ScrollScene({duration: 100})
+//                 .setPin("#my-sticky-element") // pins the element for a scroll distance of 100px
+//                 .addTo(controller); // add scene to controller
 
-// adding multiple scenes at once
-var scene2 = new ScrollScene();
-var scene3;
-controller.addScene([
-    scene2,
-    scene3 = new ScrollScene({duration: 200}), // add scene and assign handler "scene2"
-    new ScrollScene({offset: 20}) // add anonymous scene
-]);
+// // adding multiple scenes at once
+// var scene2 = new ScrollScene();
+// var scene3;
+// controller.addScene([
+//     scene2,
+//     scene3 = new ScrollScene({duration: 200}), // add scene and assign handler "scene2"
+//     new ScrollScene({offset: 20}) // add anonymous scene
+// ]);
